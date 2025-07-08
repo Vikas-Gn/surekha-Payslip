03fde6c29ef9a4959bb68c0c5000d29ca776e911
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-08 14:03:21 +0530

    updated-payslip

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3036;
+const port = 3607;
 
 // PostgreSQL database connection
 const pool = new Pool({
@@ -114,5 +114,5 @@ app.get('/api/payslips/:empCode', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://52.66.249.207:${port}`);
+    console.log(`Server running on http://3.85.61.23:${port}`);
 });
